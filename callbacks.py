37c9900fa6e799d2e7edfb5305c8b748d7618e7e
Jens Fischer <jens.fischer@danfoss.com>
2023-10-02 09:34:02 +0200

    lights incl brake and blink

---
+++ callbacks.py
@@ -0,0 +1,29 @@
+from machine import Timer
+from machine import Pin
+
+led = Pin(2, Pin.OUT) # enable onboard led as output to drive the LED
+status = 0
+
+taskList = []
+
+def tick(timer):
+    results = [f() for f in taskList]
+    
+
+def addCb(rutine):
+    taskList.append(rutine)
+
+
+def cb1():
+    global status
+    global led
+    if (status == 1):
+        led.off()
+        status = 0
+    else:
+        led.on()
+        status = 1
+ 
+tim1 = Timer(1)
+tim1.init(period=500, mode=Timer.PERIODIC, callback=tick )
+addCb(cb1)
